f46fc9259f23ae75032b5bf667ae017a772360be
Carolina Denisse Paleari <palearicaro@gmail.com>
2023-10-06 04:38:53 -0300

    Arreglo el bug.

--- src/app/pages/vision/vision.component.ts
+++ src/app/pages/vision/vision.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-vision',
@@ -6,77 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./vision.component.scss'],
 })
 export class VisionComponent {
-  markers!: number[];
+  index!: number
+  container!: any
+  markers!: number[]
+  isAnimating!: boolean
+  isTransitioning!: boolean
 
   ngOnInit() {
-    this.horizontalScroll();
+    this.index = 0;
+    this.container = document.querySelectorAll('.vision-container');
+    this.markers = new Array(this.container.length);
+    this.isAnimating = false;
+    this.isTransitioning = false;  
   }
 
-  horizontalScroll() {    
-    const container = document.querySelectorAll('.vision-container');
-    this.markers = new Array(container.length);
-    let index = 0;
-    let isAnimating = false;
-    let isTransitioning = false;
+  @HostListener('window:wheel', ['$event'])
+  onWheel(e: WheelEvent) {
+    if (!this.isAnimating && !this.isTransitioning) {
+      let nextIndex!: number;
 
-    // console.log(index);
-
-
-    document.addEventListener('wheel', function (e) {
-      if (!isAnimating && !isTransitioning) {
-        let nextIndex!: number;
-
-        if (e.deltaY > 0) {
-          nextIndex = index + 1;
-        } else if (e.deltaY < 0) {
-          nextIndex = index - 1;
-        }
-
-        if (nextIndex >= 0 && nextIndex < container.length) {
-          console.log(index);
-          console.log(nextIndex);
-
-          const currentContainer = container[index] as HTMLElement;
-          const nextContainer = container[nextIndex] as HTMLElement;
-          const markers = document.querySelectorAll('.marker');    
-          console.log(markers);
-          
-          const currentMarker = document.querySelector(
-            '.current-marker'
-          ) as HTMLElement;
-          const nextMarker = markers[nextIndex] as HTMLElement;
-          isAnimating = true;
-          isTransitioning = true;
-
-          console.log(nextMarker);
-          
-
-          currentContainer.classList.add('animateScroll');
-          currentContainer.style.opacity = '0';
-          currentMarker.classList.remove('current-marker');
-
-          currentContainer.addEventListener('transitionend', function () {
-            currentContainer.classList.remove('animateScroll');
-            currentContainer.classList.add('hidden');
-            isTransitioning = false;
-          });
-
-          setTimeout(function () {
-            nextContainer.classList.remove('hidden');
-            nextContainer.classList.add('animateScroll');
-            nextContainer.style.opacity = '1';
-            nextMarker.classList.add('current-marker');
-
-            nextContainer.addEventListener('transitionend', function () {
-              nextContainer.classList.remove('animateScroll');
-              isAnimating = false;
-            });
-          }, 700);
-
-          index = nextIndex;
-        }
+      if (e.deltaY > 0) {
+        nextIndex = this.index + 1;
+      } else if (e.deltaY < 0) {
+        nextIndex = this.index - 1;
       }
-    });
+
+      if (nextIndex >= 0 && nextIndex < this.container.length) {
+        const currentContainer = this.container[this.index] as HTMLElement;
+        const nextContainer = this.container[nextIndex] as HTMLElement;
+        const markers = document.querySelectorAll('.marker');
+        const currentMarker = document.querySelector(
+          '.current-marker'
+        ) as HTMLElement;
+
+        const nextMarker = markers[nextIndex] as HTMLElement;
+        this.isAnimating = true;
+        this.isTransitioning = true;
+
+        currentContainer.classList.add('animateScroll');
+        currentContainer.style.opacity = '0';
+        currentMarker.classList.remove('current-marker');
+
+        currentContainer.addEventListener('transitionend', () => {
+          currentContainer.classList.remove('animateScroll');
+          currentContainer.classList.add('hidden');
+          this.isTransitioning = false;
+        });
+
+        setTimeout(() => {
+          nextContainer.classList.remove('hidden');
+          nextContainer.classList.add('animateScroll');
+          nextContainer.style.opacity = '1';
+          nextMarker.classList.add('current-marker');
+
+          nextContainer.addEventListener('transitionend', () => {
+            nextContainer.classList.remove('animateScroll');
+            this.isAnimating = false;
+          });
+        }, 700);
+
+        this.index = nextIndex;
+      }
+    }
   }
 
   markerType(N: number) {
