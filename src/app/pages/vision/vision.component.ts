f5969913effad57c356d8fa2e0e17ac33e0e35b3
Carolina Denisse Paleari <palearicaro@gmail.com>
2023-10-06 03:53:22 -0300

    Scroll listo, hay bugs.

--- src/app/pages/vision/vision.component.ts
+++ src/app/pages/vision/vision.component.ts
@@ -6,47 +6,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./vision.component.scss'],
 })
 export class VisionComponent {
+  markers!: number[];
+
   ngOnInit() {
     this.horizontalScroll();
   }
 
-  horizontalScroll() {
-    // Obtén el contenedor de desplazamiento horizontal
-    const container = document.querySelector(
-      '.vision-container'
-    ) as HTMLElement;
+  horizontalScroll() {    
+    const container = document.querySelectorAll('.vision-container');
+    this.markers = new Array(container.length);
+    let index = 0;
+    let isAnimating = false;
+    let isTransitioning = false;
 
-    // Variables para el seguimiento de la dirección del desplazamiento
-    let previousScrollY = 0;
+    // console.log(index);
 
-    // Agrega un evento de desplazamiento al documento
-    document.addEventListener('wheel', (e) => {
-      // Verifica si el desplazamiento es hacia abajo
-      if (e.deltaY > 0) {
-        // Desplazamiento hacia la derecha
-        container.scrollLeft += 20; // Ajusta la velocidad de desplazamiento según tus necesidades
-      } else if (e.deltaY < 0) {
-        // Desplazamiento hacia la izquierda
-        container.scrollLeft -= 20; // Ajusta la velocidad de desplazamiento según tus necesidades
+
+    document.addEventListener('wheel', function (e) {
+      if (!isAnimating && !isTransitioning) {
+        let nextIndex!: number;
+
+        if (e.deltaY > 0) {
+          nextIndex = index + 1;
+        } else if (e.deltaY < 0) {
+          nextIndex = index - 1;
+        }
+
+        if (nextIndex >= 0 && nextIndex < container.length) {
+          console.log(index);
+          console.log(nextIndex);
+
+          const currentContainer = container[index] as HTMLElement;
+          const nextContainer = container[nextIndex] as HTMLElement;
+          const markers = document.querySelectorAll('.marker');    
+          console.log(markers);
+          
+          const currentMarker = document.querySelector(
+            '.current-marker'
+          ) as HTMLElement;
+          const nextMarker = markers[nextIndex] as HTMLElement;
+          isAnimating = true;
+          isTransitioning = true;
+
+          console.log(nextMarker);
+          
+
+          currentContainer.classList.add('animateScroll');
+          currentContainer.style.opacity = '0';
+          currentMarker.classList.remove('current-marker');
+
+          currentContainer.addEventListener('transitionend', function () {
+            currentContainer.classList.remove('animateScroll');
+            currentContainer.classList.add('hidden');
+            isTransitioning = false;
+          });
+
+          setTimeout(function () {
+            nextContainer.classList.remove('hidden');
+            nextContainer.classList.add('animateScroll');
+            nextContainer.style.opacity = '1';
+            nextMarker.classList.add('current-marker');
+
+            nextContainer.addEventListener('transitionend', function () {
+              nextContainer.classList.remove('animateScroll');
+              isAnimating = false;
+            });
+          }, 700);
+
+          index = nextIndex;
+        }
       }
-
-      // Evita el comportamiento predeterminado de la rueda del mouse
-    }); // Cambia la configuración a no pasivo
-
-    // Detecta el desplazamiento vertical para cambiar la dirección
-    window.addEventListener('scroll', () => {
-      const currentScrollY = window.scrollY;
-
-      if (currentScrollY > previousScrollY) {
-        // Desplazamiento hacia abajo, cambia la dirección a la derecha
-        container.scrollLeft += 20; // Ajusta la velocidad de desplazamiento según tus necesidades
-      } else if (currentScrollY < previousScrollY) {
-        // Desplazamiento hacia arriba, cambia la dirección a la izquierda
-        container.scrollLeft -= 20; // Ajusta la velocidad de desplazamiento según tus necesidades
-      }
-
-      // Actualiza la posición anterior del desplazamiento vertical
-      previousScrollY = currentScrollY;
     });
+  }
+
+  markerType(N: number) {
+    if (N == 0) {
+      return 'current-marker marker';
+    } else {
+      return 'marker';
+    }
   }
 }
