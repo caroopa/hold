5f25704591cc773a7ab04ebc90f2de3ce59baf3b
Carolina Denisse Paleari <palearicaro@gmail.com>
2023-12-02 23:24:01 -0300

    Listo hover letter.

--- src/app/components/hover-letter/hover-letter.component.ts
+++ src/app/components/hover-letter/hover-letter.component.ts
@@ -7,29 +7,19 @@ import gsap from 'gsap';
   styleUrls: ['./hover-letter.component.scss'],
 })
 export class HoverLetterComponent {
-  // @Input() text!: string;
-  // @Input() color!: string;
-
   ngAfterViewInit() {
     this.hoverLetterAnimation();
   }
 
   hoverLetterAnimation() {
-    // const container = document.getElementById('hover-container') as HTMLElement  ;
-
     document.body.addEventListener('mousemove', (evt) => {
       const mouseX = evt.clientX;
       const mouseY = evt.clientY;
 
-      // gsap.set(".cursor", {
-      //   x: mouseX,
-      //   y: mouseY
-      // })
-
       gsap.to('.shape', {
         x: mouseX,
         y: mouseY,
-        stagger: -0.1,
+        stagger: -0.07,
       });
     });
   }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,31 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  
-  ngAfterViewInit() {
-    this.hoverLetterAnimation();
-  }
-
-  hoverLetterAnimation() {
-    // const container = document.getElementById('hover-container') as HTMLElement  ;
-
-    document.body.addEventListener('mousemove', (evt) => {
-      const mouseX = evt.clientX;
-      const mouseY = evt.clientY;
-
-      // gsap.set(".cursor", {
-      //   x: mouseX,
-      //   y: mouseY
-      // })
-
-      gsap.to('.shape', {
-        x: mouseX,
-        y: mouseY,
-        stagger: -0.1,
-      });
-    });
-  }
-}
+export class HomeComponent {}
