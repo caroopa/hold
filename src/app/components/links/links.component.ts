b9b79c3866ca857e32535dd808848710c26b84bd
Carolina Denisse Paleari <palearicaro@gmail.com>
2023-12-13 22:39:30 -0300

    Links animation.

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
-  styleUrls: ['./links.component.scss']
+  styleUrls: ['./links.component.scss'],
 })
 export class LinksComponent {
+  showFollow = false;
+  hideFollow = true;
+  showHello = false;
 
+  toggleFollowPopup() {
+    this.showFollow = !this.showFollow;
+    this.hideFollow = !this.hideFollow;
+  }
+
+  helloPopup() {
+    this.showHello = true;
+    setTimeout(() => {
+      this.showHello = false;
+    }, 1850);
+  }
 }
